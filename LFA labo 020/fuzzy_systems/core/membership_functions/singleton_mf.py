f4adc089caea500e683c82cb5bf6ad60075ae9a0
GaussianBlurs <arnoldvbg@gmail.com>
2019-03-13 00:34:27 +0100

    Submission

--- LFA labo 020/fuzzy_systems/core/membership_functions/singleton_mf.py
+++ LFA labo 020/fuzzy_systems/core/membership_functions/singleton_mf.py
@@ -3,17 +3,24 @@ import numpy as np
 
 class SingletonMF(FreeShapeMF):
     def __init__(self, x):
+        """
+        Build a singleton membership function by defining the only input value (x)
+        wwhere the corresponding output values is 1
+
+        This class is the most basic way available to create singleton membership
+        functions.
+
+        :param x: x value of the singleton membership function where it's equal to 1, the rest is 0
+        """
         self.x = x
         y = 1
         args = [x], [y] 
         super().__init__(*args)
 
     def fuzzify(self, in_value):
-
+        # return the 0 if the given in_value is not equal to the self.x
         mf = 0
-
         if(self.x == in_value):
             mf = 1
 
         return np.interp(in_value, self._in_values, [mf])
-
--- LFA labo 020/fuzzy_systems/core/membership_functions/trap_mf.py
+++ LFA labo 020/fuzzy_systems/core/membership_functions/trap_mf.py
@@ -2,5 +2,25 @@ from fuzzy_systems.core.membership_functions.lin_piece_wise_mf import LinPWMF
 
 class TrapMF(LinPWMF):
     def __init__(self, p0, p1, p2, p3):
+        
+        """
+        Build a trapezoidal membership function by defining input values (x axis,  [p0, p1, p2, p3])
+        and corresponding output values (y axis, [0.0, 1.0, 1.0, 0.0])
+
+        This class is the most basic way available to create trapezoidal membership
+        functions.
+
+        :param p0: first vertice of the trapezoidal membership function
+        :param p1: second vertice of the trapezoidal membership function
+        :param p2: third vertice of the trapezoidal membership function
+        :param p3: fourth vertice of the trapezoidal membership function
+        """
+
+        # We test if the trapezoidal membership function is not concave
+        assert p3 >= p2, "Input p3 should be greater than or equal to p2"
+        assert p2 >= p1, "Input p2 should be greater than or equal to p1"
+        assert p1 >= p0, "Input p1 should be greater than or equal to p0"
+
+        # We force the output values to be [0.0, 1.0, 1.0, 0.0] so the trapezoidal membership function is well designed
         args = [p0, 0.0], [p1, 1.0], [p2, 1.0], [p3, 0.0]
         super().__init__(*args)
